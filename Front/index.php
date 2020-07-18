fa80d772765bb865c6f6ba1bca6ae807e703c3cb
Beto <betohurtado3@hotmail.com>
2020-07-18 16:11:26 -0500

    Creación de la pagina principal (Index)

---
+++ Front/index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Yu-gi-oh! Card Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/heroic-features.css" rel="stylesheet">
+    <link rel="icon" href="../images/icon.png">
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #C12705;">
+        <div class="container">
+           <img src="../images/yugi.png" height="50">
+            <a class="navbar-brand" href="#">Yu-Gi-Oh! - Fan Data Page</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cartas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Decks</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Manga/Anime</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Información</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header class="jumbotron my-4">
+            <img src="../images/home.jpg">
+        </header>
+
+        <!-- Menu buttons -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="text-center">
+                    <button type="button" class="btn btn-outline-danger">Noticias</button>
+                    <button type="button" class="btn btn-outline-danger">Eventos</button>
+                    <button type="button" class="btn btn-outline-danger">Reglas</button>
+                    <button type="button" class="btn btn-outline-danger">Banlist</button>
+                </div>
+            </div>
+        </div>
+
+        <br>
+        <hr>
+        <!-- Page Features -->
+        <div class="row text-center">
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img class="card-img-top" src="../images/draon.jpg">
+                    <div class="card-body">
+                        <h4 class="card-title">Dragón Blanco de Ojos Azules</h4>
+                        <p class="card-text">Este legendario dragón es una poderosa máquina de destrucción. Virtualmente invencible, muy pocos se han enfrentado a esta impresionante criatura y han vivido para contarlo.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img class="card-img-top" src="../images/Foto_mago_oscuro.jpg">
+                    <div class="card-body">
+                        <h4 class="card-title">Mago Oscuro</h4>
+                        <p class="card-text">El más grande de los magos en cuanto al ataque y la defensa.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img class="card-img-top" src="../images/Foto_drag%253Fn_negro_de_ojos_rojos.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">Dragón Negro de Ojos Rojos</h4>
+                        <p class="card-text">Un dragón feroz con un ataque mortal.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card h-100">
+                    <img class="card-img-top" src="../images/Foto_cr%253Fneo_convocado.jpg" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">Cráneo Convocado</h4>
+                        <p class="card-text">Un demonio con poderes oscuros para confundir al enemigo. Entre los monstruos de Tipo Demonio, este monstruo alardea de considerable fuerza.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+    
+    
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Yu-Gi-Oh! <br>(遊 戯 王, Yū-gi-ō;? «El rey de los juegos») </h1>
+    <p class="lead">Es un manga creado por Kazuki Takahashi, que ha dado lugar a una franquicia, además de múltiples series de anime, juegos de cartas y numerosos videojuegos.</p>
+    <a href="#" class="btn btn-primary">Información completa</a>
+  </div>
+</div>
+   
+   
+  
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright Konami &copy; All rights reserved</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
